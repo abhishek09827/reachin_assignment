ab215fd5392c1b76725feca55f26527b9411ae52
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2024-06-26 01:46:26 +0530

    Node app initialized

---
+++ server/src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { apiRouter } from './routes/api.routes';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware and configuration settings
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Routes declaration
+app.use("/api/v1/users", apiRouter);
+
+export { app };
---
+++ server/src/routes/api.routes.ts
@@ -0,0 +1,2 @@
+const apiRouter = ""
+export {apiRouter}
